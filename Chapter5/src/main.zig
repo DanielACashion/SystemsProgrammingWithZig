86732e3b56e190d90481ce1fdf85bb8badf197f4
Cashion <danielacashion@gmail.com>
2026-06-26 04:56:56 -0500

    wrapping up work for the night on reading.
    leaving off at 5.3.2

--- Chapter5/src/main.zig
+++ Chapter5/src/main.zig
@@ -1,5 +1,31 @@
 const std = @import("std");
 
+var gpa: std.heap.DebugAllocator(.{}) = .init;
+export fn recipes_quit() void {
+    _ = gpa.deinit();
+    gpa = .init;
+}
+const RecipeBook = struct {
+    const Self = @This();
+    fn deinit(allocator: std.mem.Allocator) void {
+        _ = allocator;
+    }
+};
+export fn recipe_book_create() ?*RecipeBook {
+    const allocator = gpa.allocator();
+    const book = allocator.create(RecipeBook) catch {
+        return null;
+    };
+    book.* = .{};
+    return book;
+}
+
+export fn recipe_book_close(book: *RecipeBook) void {
+    const allocator = gpa.allocator();
+    book.*.deinit(allocator);
+    allocator.destroy(book);
+}
+
 pub fn main(init: std.process.Init) !void {
     var write_buf: [1024]u8 = undefined;
     const stdout_writer = std.Io.File.stdout().writer(init.io, &write_buf);
@@ -8,3 +34,14 @@ pub fn main(init: std.process.Init) !void {
     try stdout.print("Starting Chapter 5\n", .{});
     try stdout.flush();
 }
+
+//EXAMPLES
+const Entity = enum(u32) {
+    _,
+};
+const EntityKind = enum { player, enemy };
+
+const EntityTable = struct {
+    kinds: std.ArrayList(EntityKind) = .empty,
+    positions: std.ArrayList([2]i32) = .empty,
+};
